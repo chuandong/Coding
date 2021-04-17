1be6a31bbc91e617b38e424569a57e0212c2a9d9
chuandong <1165532311@qq.com>
2021-04-18 00:15:45 +0800

    Á´±í²Ù×÷

--- static_link.c
+++ static_link.c
@@ -3,7 +3,7 @@
 typedef struct node{
     int data;
     struct node *next;
-}nodelist;
+}nodelist, *linklist;
 
 void init_list(nodelist **L)
 {
@@ -14,15 +14,56 @@ void init_list(nodelist **L)
     return ;
 }
 
+int pos_list(nodelist *L, int pos)
+{
+    int i = 0;
+    
+    nodelist *p = L;
+    
+    while (p && i < pos)
+    {
+        p = p->next;
+        i++;
+    }
+    
+    if (i < pos)
+    {
+        printf("not found\n");
+        return -1;
+    }
+    
+    return p->data;
+}
+
+int find_max_num_list(nodelist *L)
+{
+    int maxdata = 0;
+    
+    nodelist *p = L->next;
+    
+    while (p)
+    {
+        if (maxdata < p->data)
+            maxdata = p->data;
+        p = p->next; 
+    }
+    
+    printf("最大的值为:[%d]\n", maxdata);
+    
+    return maxdata;
+}
+
 int main()
 {
     int i = 0;
     
     nodelist *list, *rlist;
     nodelist *tmp;
+    int pos;
     
     int A[9] = {1,2,3,4,5,6,7,8,9};
     
+    /*初始化头结点*/
     init_list(&list);
     
     rlist = list; /*尾插法使用*/
@@ -40,6 +81,7 @@ int main()
         rlist = tmp;
     }
     
+    /*输出链表*/
     nodelist *p;
     p = list->next;
     
@@ -50,5 +92,13 @@ int main()
     }
     
     printf("-->nul\n");
+    
+    /*查找位置的元素*/
+    pos = pos_list(list, 3);
+    printf("pos:%d, address:%p\n", pos);
+    
+    /*查找队列中最大的值*/
+    pos = find_max_num_list(list);
+    printf("pos:%d, address:%p\n", pos);
     return 0;
 }
